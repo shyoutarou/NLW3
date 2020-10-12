9b6cf2c11a47f3ca40d0ee6f834d73d085523183
brenosekiro <b88660501@gmail.com>
2020-10-12 12:52:02 -0300

    orphanagesMap-panel

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import mapMakerImg from '../images/marker.svg'
+import { FiPlus } from 'react-icons/fi'
+import '../styles/pages/orphanagesMap.css'
 
 const OrphanagesMap = () => {
     return (
-        <h1>Hello world</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMakerImg} alt="marker"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Rio do sul</strong>
+                    <span>Santa Catarina</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
     )
 }
 
